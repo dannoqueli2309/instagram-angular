01cbafcf1ae1b9cb48af2266fc1829d165c2e7ea
DANILO NOQUELI DE OLIVEIRA <danilo.n.oliveira@bradesco.com.br>
2021-01-30 16:19:20 -0300

    e linkando o componente login e cadastro

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -14,16 +14,30 @@ import { Component, OnInit } from '@angular/core';
         style({opacity:0, transform: 'translate(-50px,0px)'}),
         animate('500ms 0s ease-in-out'), // duração, delay e aceleração(easy)
       ])
+    ]),
+    trigger('animation-painel',[
+      state('criado',style({
+      opacity:1
+    })),
+    transition('void=>criado',[
+      style({opacity:0, transform: 'translate(50px,0px)'}),
+      animate('500ms 0s ease-in-out'),
     ])
+  ])
   ]
 })
 export class acessoComponent implements OnInit {
 
   public estadoBanner = 'criado';
-
-  constructor() { }
+  public estadoPainel = 'criado';
+  public cadastro = false;
+   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public exibirPainel(eventValue:string):void{
+    this.cadastro = eventValue === 'cadastro' ? true : false;
   }
 
 }
