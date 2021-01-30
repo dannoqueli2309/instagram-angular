f0f6434ed4254d20f24f0b79fe44ce739f99904b
DANILO NOQUELI DE OLIVEIRA <danilo.n.oliveira@bradesco.com.br>
2021-01-30 15:33:25 -0300

    animação do banner

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,25 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations:[
+    trigger('animacao-banner',[
+      state('criado',style({
+        opacity:1
+      })),
+      transition('void=>criado',[
+        style({opacity:0, transform: 'translate(-50px,0px)'}),
+        animate('500ms 0s ease-in-out'), // duração, delay e aceleração(easy)
+      ])
+    ])
+  ]
 })
 export class acessoComponent implements OnInit {
+
+  public estadoBanner = 'criado';
 
   constructor() { }
 
