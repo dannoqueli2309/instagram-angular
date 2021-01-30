903a9c7497c2e7fbd751b901a85b4d6daa968d3e
DANILO NOQUELI DE OLIVEIRA <danilo.n.oliveira@bradesco.com.br>
2021-01-30 15:19:49 -0300

    adicionando animação rotativa no banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,58 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-
+import { Imagem } from './imagem.model'
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state(
+        'escondido',
+        style({
+          opacity: 0,
+        })),
+      state(
+        'visivel',
+        style({
+          opacity: 1,
+        })),
+        // Esse sintaxe(<=>) é usado para mudar a transição de visivel para escondido e vice e versa 
+        transition('escondido <=> visivel', animate('1s ease-in'))
+    ]),
+  ],
 })
 export class BannerComponent implements OnInit {
+  public estado: string = 'visivel';
+  
+  public imagens:Imagem[] = [
+    {estado:'visivel', url:'/assets/banner-acesso/img_1.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_2.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_3.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_4.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_5.png'},
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(()=>this.logicaRotação(),3000);
+  }
+
+  public logicaRotação(): void{
+    let idx: number;
+
+    // ocutar imagem
+    for(let i: number=0; i<=4;i++){
+      if(this.imagens[i].estado == 'visivel'){
+        this.imagens[i].estado = 'escondido';
+        idx = i === 4 ? 0 : i+1;
+        break;
+      }
+    }
+    // exibir imagem
+      this.imagens[idx].estado = 'visivel'
+    setTimeout(()=>this.logicaRotação(),3000);
   }
 
 }
